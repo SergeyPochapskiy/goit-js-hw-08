34aec4fe69931d2fed1cd2bd5ab520b01d201f23
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2022-09-26 22:02:12 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
+const player = new Player('vimeo-player');
+const LOCALSTORAGE_PLAYER_KEY = 'videoplayer-current-time';
+const refPlayer = document.querySelector('#vimeo-player');
+
+player.on('timeupdate', throttle(data, 1000));
+function data(timeupdate) {
+  const seconds = timeupdate.seconds;
+  save(LOCALSTORAGE_PLAYER_KEY, seconds);
+  if (!seconds) {
+    return;
+  }
+}
+
+
+player.setCurrentTime(load(LOCALSTORAGE_PLAYER_KEY)).then(function (seconds) {
+  
+});
+
+const markup = `<button class="buttonReset" type="reset">Очистити час перегляду</button>`;
+refPlayer.insertAdjacentHTML('afterend', markup);
+
+const onResetTime = event => {
+  window.location.reload();
+  remove(LOCALSTORAGE_PLAYER_KEY);
+};
+const refButton = document.querySelector('.buttonReset');
+refButton.addEventListener('click', onResetTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+import { load, save, remove } from './storage';
 
+const LOCALSTORAGE_FORM_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+
+initPage();
+
+const onFormInput = event => {
+  const { name, value } = event.target;
+  let saveData = load(LOCALSTORAGE_FORM_KEY);
+  saveData = saveData ? saveData : {};
+  saveData[name] = value;
+  save(LOCALSTORAGE_FORM_KEY, saveData);
+};
+
+const throttleOnFormInput = throttle(onFormInput, 500);
+formRef.addEventListener('input', throttleOnFormInput);
+
+function initPage() {
+  const saveData = load(LOCALSTORAGE_FORM_KEY);
+  if (!saveData) {
+    return;
+  }
+  Object.entries(saveData).forEach(([name, value]) => {
+    formRef.elements[name].value = value;
+  });
+}
+// -------------------------------------------
+const onSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  event.currentTarget.reset();
+  remove(LOCALSTORAGE_FORM_KEY);
+};
+
+formRef.addEventListener('submit', onSubmit);
